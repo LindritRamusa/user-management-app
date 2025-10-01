ca147ab625724a6b713157a3ffc486ac4cdba522
Lindrit Ramusa <lindrit.ramusa5@gmail.com>
2025-10-01 20:39:17 +0200

    feat: added neccessary components and routing to each one of them

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import {Routes, Route } from 'react-router-dom'
+import UsersList from './components/UsersList.jsx'
+import UserDetails from './components/UserDetails.jsx'
 function App() {
 
   return (
-    <section>
-      <div className="bg-red-500 text-green-600">User Management App</div>
-    </section>
+   <Routes>
+    <Route path='/' element={UsersList()}/>
+    <Route path='/user/:id' element={UserDetails()}/>
+   </Routes>
   )
 }
 
