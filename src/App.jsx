5c356fc1a0cc9ddff1ce54d83f70becf95ce645a
Lindrit Ramusa <lindrit.ramusa5@gmail.com>
2025-10-01 22:55:19 +0200

    feat: fetch users and list them without styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import {Routes, Route } from 'react-router-dom'
-import UsersList from './components/UsersList.jsx'
-import UserDetails from './components/UserDetails.jsx'
+import { Routes, Route } from "react-router-dom";
+import UsersList from "./components/UsersList.jsx";
+import UserDetails from "./components/UserDetails.jsx";
 function App() {
-
   return (
-   <Routes>
-    <Route path='/' element={UsersList()}/>
-    <Route path='/user/:id' element={UserDetails()}/>
-   </Routes>
-  )
+    <Routes>
+      <Route path="/" element={UsersList()} />
+      <Route path="/user/:id" element={UserDetails()} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
-
+import React, { use } from "react";
+import { useState, useEffect } from "react";
 function UsersList() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+ 
+  async function fetchUsers() {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch the users");
+      }
+      const data = await response.json();
+      setUsers(data);
+      setError(null);
+    } catch (error) {
+      setError.message;
+    } finally {
+      setLoading(false);
+    }
+  }
+
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center">
+        <div className="text-xl font-semibold text-gray-600">
+          Loading users...
+        </div>
+      </div>
+    );
+  }
+  if(error){
+    return (
+      <div className="flex items-center justify-center">
+        <div className="text-xl font-semibold text-red-600">Error: {error}</div>
+      </div>
+    )
+  }
+
   return (
-    <div>UsersList</div>
+    <div className="min-h-screen bg-gray-50 py-8">
+      {users.map((user)=>(
+        <div key={user.id}>
+          {user.name}
+          {user.email}
+          {user.company?.name || 'N/A'}
+          {user.id}
+        </div>
+      ))}
+    </div>
   )
 }
 
-export default UsersList
+export default UsersList;
